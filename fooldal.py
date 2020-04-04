6d915ae3c34042c618cf4f6551c0f0f73e3ab32c
bori65 <bori1000001@gmail.com>
2020-04-04 11:33:52 +0200

    par valtoztatas

--- fooldal.py
+++ fooldal.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -6,16 +6,25 @@ Bootstrap(app)
 
 @app.route('/')
 def login():
-    return render_template("bejelentkezes2.html")
+    name = request.args.get('name')
+    password = request.args.get('password')
+    if name and password:
+        return render_template('chat.html', name = name, password = password)
+    else:
+        return render_template("bejelentkezes2.html")
+
+
+    #return render_template("bejelentkezes2.html")
 	
-@app.route('/chat')
+@app.route('/chat/')
 def chat():
     name = request.args.get('name')
-	password = request.args.get('password')
-	if name and password:
-		return render_template('chat.html', name = name, password = password)
-	else:
-		return redirect(url_for('login'))
+    password = request.args.get('password')
+    print(f"name: {name} password: {password}")
+    if name and password:
+        return render_template('chat.html', name = name, password = password)
+    else:
+        return redirect(url_for('regisztracio2'))
 
 		
 if __name__ == "__main__":
